f1d0800d07f4b4ebc2d6999d15847d7093fc1234
Sammi Chekroud <sammi.chekroud@psych.ox.ac.uk>
2018-01-16 19:49:18 +0000

    add initial draft of parsing function, now much easier through vectorisation with numpy. scales to Sages data too

--- myfunctions.py
+++ myfunctions.py
@@ -10,8 +10,7 @@ import numpy   as np
 import scipy   as sp
 from scipy import interpolate
 import sys
-#from numpy import NaN, Inf, arange, isscalar, asarray, array
-
+import os
 
 def Eucdist(x1, y1, x2, y2):
     """
@@ -22,7 +21,182 @@ def Eucdist(x1, y1, x2, y2):
     """
     distance = np.sqrt( (x2-x1)**2 + (y2-y1)**2)
     return distance
- 
+
+
+def parse_eye_data(eye_fname, block_folder,nblocks, ntrials):
+    
+    """
+    eye_fname expects the full path to a file that needs to be parsed.
+        The function will take the name of this, minus the file extension, and create a parsed pickle version
+    
+    out_folder needs to be a path to where you want the parsed data to be output to.
+    
+    if you give it os.path.join(some_path, 'fname.asc') it will save:
+    os.path.join(block_folder, 'fname_parsed.pickle'). These pickle files are read into later preprocessing functions
+    """
+    
+    d = open(eye_fname, 'r')
+    raw_d = d.readlines()
+    d.close()
+    
+    split_d = []
+    for i in range(len(raw_d)):
+        tmp = raw_d[i].split()
+        split_d.append(tmp)
+    
+    #get all lines where 'START'i s seen, as this marks the start of the recording
+    start_inds = [x for x in range(len(split_d)) if len(split_d[x]) == 6 and split_d[x][0] == 'START']
+    #get points where recording stopped
+    end_inds   = [x for x in range(len(split_d)) if len(split_d[x]) == 7 and split_d[x][0] == 'END']
+    
+    #assign some empty lists to get filled with information
+    trackertime = []
+    lx   = []; rx   = []
+    ly   = []; ry   = []
+    lp   = []; rp   = []
+    av_x = []; av_y = []
+    av_p = []
+    
+    Efix = []; Sfix = []
+    Esac = []; Ssac = []
+    Eblk = []; Sblk = []
+    Msg  = []
+    
+    for i in range(len(start_inds)):
+        start_line = start_inds[i]
+        end_line   = end_inds[i]
+        
+        itrl = np.array(split_d[start_line:end_line])
+        
+        itrl_event_inds = [x for x in range(len(itrl)) if itrl[x][0] == 'MSG']        # get the line indices where trigs sent
+        itrl_events     = [itrl[x] for x in itrl_event_inds]                              # get the actual triggers
+        itrl_fix_inds   = [x for x in range(len(itrl)) if itrl[x][0] == 'EFIX' or itrl[x][0] == 'SFIX']
+        itrl_fix        = [itrl[x] for x in itrl_fix_inds]
+        itrl_sac_inds   = [x for x in range(len(itrl)) if itrl[x][0] == 'ESACC' or itrl[x][0] == 'SSACC']
+        itrl_sac        = [itrl[x] for x in itrl_sac_inds]
+        itrl_blink_inds = [x for x in range(len(itrl)) if itrl[x][0] == 'EBLINK' or itrl[x][0] == 'SBLINK']
+        itrl_blink      = [itrl[x] for x in itrl_blink_inds]
+        
+        itrl_nondata    = sorted(itrl_blink_inds + itrl_sac_inds + itrl_fix_inds + itrl_event_inds) #get all non-data line indices
+        itrl_data = np.delete(itrl, itrl_nondata) #remove these lines so all you have is the raw data
+
+        itrl_data = itrl_data[6:] #remove first five lines as these are filler after the recording starts        
+        
+        itrl_data = np.vstack(itrl_data) #shape of this should be number of columns in the file (data)
+        
+        #before you can convert to float, need to replace missing data where its '.' as nans (in pupil this is '0.0')
+        eyecols = [1,2,4,5] #leftx, lefty, rightx, right y col indices
+        for col in eyecols:
+            missing_inds = np.where(itrl_data[:,col] == '.') #find where data is missing in the gaze position, as probably a blink (or its missing as lost the eye)
+            for i in missing_inds:
+                itrl_data[i,col] = np.NaN #replace missing data ('.') with NaN
+                itrl_data[i,3]   = np.NaN #replace left pupil as NaN (as in a blink)
+                itrl_data[i,6]   = np.NaN #replace right pupil as NaN (as in a blink)
+        itrl_data = itrl_data.astype(np.float) #convert data from string to floats for computations
+         
+        #for binocular data, the shape is:
+        # columns: time stamp, left x, left y, left pupil, right x, right y, right pupil
+        itrl_trackertime = itrl_data[:,0]
+        itrl_lx, itrl_ly, itrl_lp = itrl_data[:,1], itrl_data[:,2], itrl_data[:,3]
+        itrl_rx, itrl_ry, itrl_rp = itrl_data[:,4], itrl_data[:,5], itrl_data[:,6]
+        
+        #average data across the eyes --- take the nanmean though in case of missing data. we'll still save the independent eyes though as a sanity check
+        itrl_x = np.vstack([itrl_lx, itrl_rx])
+        itrl_x = np.nanmean(itrl_x, axis = 0)
+        itrl_y = np.vstack([itrl_ly, itrl_ry])
+        itrl_y = np.nanmean(itrl_y, axis = 0)    
+        itrl_p = np.vstack([itrl_lp, itrl_rp])
+        itrl_p = np.nanmean(itrl_p, axis = 0)
+    
+        # split Efix/Sfix and Esacc/Ssacc into separate lists
+        itrl_efix = [itrl_fix[x] for x in range(len(itrl_fix)) if
+                     itrl_fix[x][0] == 'EFIX']
+        itrl_sfix = [itrl_fix[x] for x in range(len(itrl_fix)) if
+                     itrl_fix[x][0] == 'SFIX']
+                    
+        itrl_ssac = [itrl_sac[x] for x in range(len(itrl_sac)) if
+                     itrl_sac[x][0] == 'SSACC']
+        itrl_esac = [itrl_sac[x] for x in range(len(itrl_sac)) if
+                     itrl_sac[x][0] == 'ESACC']
+                     
+        itrl_sblk = [itrl_blink[x] for x in range(len(itrl_blink)) if
+                     itrl_blink[x][0] == 'SBLINK']
+        itrl_eblk = [itrl_blink[x] for x in range(len(itrl_blink)) if
+                     itrl_blink[x][0] == 'EBLINK']
+    
+        #append to the collection of all data now
+        trackertime.append(itrl_trackertime)
+        lx.append(itrl_lx)
+        ly.append(itrl_ly)
+        rx.append(itrl_rx)
+        ry.append(itrl_ry)
+        lp.append(itrl_lp)
+        rp.append(itrl_rp)
+        av_x.append(itrl_x)
+        av_y.append(itrl_y)
+        av_p.append(itrl_p)
+        Efix.append(itrl_efix)
+        Sfix.append(itrl_sfix)
+        Ssac.append(itrl_ssac)
+        Esac.append(itrl_esac)
+        Sblk.append(itrl_sblk)
+        Eblk.append(itrl_eblk)
+        Msg.append(itrl_events)    
+    
+    #rep_inds  = np.repeat(np.arange(nblocks),trialsperblock)
+    #plt.hist(rep_inds, bins = nblocks) # all bars should be the same height now if its work? @ trials per block (80)
+    
+    iblock = {
+    'trackertime': [],
+    'av_x': [], 'av_y': [], 'av_p': [],
+    'lx'  : [], 'ly'  : [], 'lp'  : [],
+    'rx'  : [], 'ry'  : [], 'rp'  : [],
+    'Efix': [], 'Sfix': [],
+    'Esac': [], 'Ssac': [],
+    'Eblk': [], 'Sblk': [],
+    'Msg' : []
+    }
+    dummy = copy.deepcopy(iblock)
+    blocked_data = np.repeat(dummy, nblocks)
+    
+    for i in np.arange(nblocks):
+        inds = np.squeeze(np.where(rep_inds == i))
+        iblock_data = copy.deepcopy(iblock)
+        for ind in inds: #add trialwise info into the blocked structure, continuous data rather than sectioned
+            iblock_data['trackertime'].append(trackertime[ind])
+            iblock_data['av_x'].append(av_x[ind])
+            iblock_data['av_y'].append(av_y[ind])
+            iblock_data['av_p'].append(av_p[ind])
+            iblock_data['lx'].append(lx[ind])
+            iblock_data['ly'].append(ly[ind])
+            iblock_data['lp'].append(lp[ind])
+            iblock_data['rx'].append(rx[ind])
+            iblock_data['ry'].append(ry[ind])
+            iblock_data['rp'].append(rp[ind])
+            iblock_data['Efix'].append(Efix[ind])
+            iblock_data['Sfix'].append(Sfix[ind])
+            iblock_data['Esac'].append(Esac[ind])
+            iblock_data['Ssac'].append(Ssac[ind])
+            iblock_data['Eblk'].append(Eblk[ind])
+            iblock_data['Sblk'].append(Sblk[ind])
+            iblock_data['Msg'].append(Msg[ind])
+            blocked_data[i] = iblock_data
+    for block in range(len(blocked_data)): #concatenate trialwise signals into whole block traces to make artefact removal easier
+        blocked_data[block]['trackertime'] = np.hstack(blocked_data[block]['trackertime'])
+        blocked_data[block]['av_x']        = np.hstack(blocked_data[block]['av_x']       )
+        blocked_data[block]['av_y']        = np.hstack(blocked_data[block]['av_y']       )
+        blocked_data[block]['av_p']        = np.hstack(blocked_data[block]['av_p']       )
+        blocked_data[block]['lx']          = np.hstack(blocked_data[block]['lx']         )
+        blocked_data[block]['ly']          = np.hstack(blocked_data[block]['ly']         )
+        blocked_data[block]['lp']          = np.hstack(blocked_data[block]['lp']         )
+        blocked_data[block]['rx']          = np.hstack(blocked_data[block]['rx']         )
+        blocked_data[block]['ry']          = np.hstack(blocked_data[block]['ry']         )
+        blocked_data[block]['rp']          = np.hstack(blocked_data[block]['rp']         )
+    return blocked_data
+
+
+
+
 def find_missing_periods(data, nblocks):
     '''
     this function will find all sections of missing data. this typically relates to blinks
